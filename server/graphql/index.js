d2f9b3cb7fcf0e578aa237c7c9aafbdc41293d18
jaketrent <trent.jake@gmail.com>
2016-08-17 16:11:15 -0600

    attempt to graphql query with arbitrary obj

--- server/graphql/index.js
+++ server/graphql/index.js
@@ -1,10 +1,14 @@
 const {
   graphql,
+  GraphQLID,
   GraphQLList,
   GraphQLObjectType,
   GraphQLSchema,
   GraphQLString
 } = require('graphql')
+const { GraphQLError } = require('graphql/error')
+const { GraphQLScalarType } = require('graphql/type')
+const { Kind } = require('graphql/language')
 const graphqlHTTP = require('koa-graphql')
 const koa = require('koa')
 const mount = require('koa-mount')
@@ -13,10 +17,24 @@ const logsRepo = require('../logs/repo')
 
 const app = koa()
 
+const payloadType = new GraphQLScalarType({
+  name: 'payload',
+  serialize: value => value,
+  parseValue: value => value,
+  parseLiteral: ast => {
+    if (ast.kind !== Kind.OBJECT)
+      throw new GraphQLError("Query error: Can only parse object but got a: " + ast.kind, [ast])
+    return ast.value
+  }
+})
+
 const logType = new GraphQLObjectType({
   name: 'logs',
   fields: {
-    message: { type: GraphQLString }
+    created: { type: GraphQLString },
+    id: { type: GraphQLID },
+    message: { type: GraphQLString },
+    payload: { type: payloadType }
   }
 })
 
@@ -24,7 +42,8 @@ const logQueryFieldConfig = {
   logs: {
     type: new GraphQLList(logType),
     args: {
-      message: { type: GraphQLString }
+      message: { type: GraphQLString },
+      payload: { type: payloadType }
     },
     resolve(_, args) {
       return logsRepo.query(args)
